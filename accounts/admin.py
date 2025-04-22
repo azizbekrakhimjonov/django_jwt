ce5b2913fcc995a37a854bfedad944ee855d2e40
azizbekrakhimjonov <azizbekrahimjonov571@gmail.com>
2025-04-23 03:22:12 +0500

    v3

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import CustomUser, Profile
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+   list_display = [field.name for field in CustomUser._meta.fields]
+
+
+@admin.register(Profile)
+class ProfilAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Profile._meta.fields]
