6a23f7338e8b6c9555a558e5ab002e7a878812e7
azizbekrakhimjonov <azizbekrahimjonov571@gmail.com>
2025-04-23 04:20:33 +0500

    v5

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,34 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 from accounts.views import RegisterView, MyTokenObtainPairView, UserListView, UserDetailView
 from api.views import PostListCreateView, PostRetrieveUpdateDestroyView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from django.contrib import admin
+from django.urls import re_path
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Registration API",
+        default_version='v1',
+        description="API documentation for the register app",
+        terms_of_service="https://t.me/calibr_i",
+        contact=openapi.Contact(email="azizbekrahimjonov571@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    re_path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
     # Auth URLs
     path('api/auth/register/', RegisterView.as_view(), name='register'),
